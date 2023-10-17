490cc448790c7c96671c4eb359eca46a02365383
Joel Chambers <104284414+Boomboomyoyo@users.noreply.github.com>
2023-10-17 12:28:09 -0600

    Not sure where to start on checking permutations

--- code.js
+++ code.js
@@ -1,3 +1,62 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(a, length, tests) {
+
+    /*if(a.length == length)
+    {
+        tests++
+        test = true
+        // Compare each value to each other value to ensure the array is sorted
+        for(i=0; i < a.length && test == true; i++)
+        {
+            for(j = i; j < a.length && test == true; j++)
+            {
+                if(a[i] > a[j])
+                {
+                    test = false
+                }
+            }
+        }
+        if(test = true)
+        {
+            return tests
+        }
+    }
+
+    else
+    {
+        permutationSort
+    }*/
+    
+    /*for(b = a.length-1; b >= 0; b--)
+    {
+        for(c = a.length-1; c >= b; c--)
+        {
+            for(d = a.length-1; d >= c; d--)
+        }
+    }*/
+    tests = 0
+    test = true
+    //for(k = array.length-1; k )
+    //{
+        // Compare each value to each other value to ensure the array is sorted
+        for(i=0; i < a.length; i++)
+        {
+            for(j = i; j < a.length; j++)
+            {
+                if(a[i] > a[j])
+                {
+                    test = false
+                }
+            }
+        }
+    //}
+    return test
 }
+
+function swapIndices(x,y,arr)
+{
+    temp = arr[x]
+    arr[x] = arr[y]
+    arr[y] = temp
+}
+
+console.log(permutationSort([2,3,5,6,5,2,3]))
